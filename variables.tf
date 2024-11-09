1e02c6005595fd682d8f83398aeebf19a94ab524
Mehdi Lotfinejad <lotfinejad@gmail.com>
2024-11-09 15:57:00 +0800

    third-commit

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC."
   type        = string
